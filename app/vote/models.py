9a893b7b384de5f7b79d9143db95eaf0f9e4725f
Dest1ni <vilo76671@gmail.com>
2023-10-23 19:31:54 +0300

    alter vote tables

--- app/vote/models.py
+++ app/vote/models.py
@@ -1,21 +1,48 @@
 from django.db import models
+from users.models import UserModel
 
 class BaseVoteFieldModel(models.Model):
-    question = models.TextField()
+    """ 
+    Базовое модель поля голосования/опроса 
+    """
+    question = models.TextField(null=False)
     vote = models.BooleanField() 
 
 class SurveyFormModel(models.Model):
+    """
+    Модель опроса
+    """
     text_answer = models.TextField()
-    # who create
-    # creates for (Optional)
+    who_create = models.ForeignKey(UserModel,on_delete=models.CASCADE,null=False)
 
 class VotingFormModel(models.Model):
-    pass
-    # who create
-    # creates for (Optional)
-    
+    """
+    Модель голосования
+    """
+    who_create = models.ForeignKey(UserModel,on_delete=models.CASCADE,null=False)
+
 class VoteFieldModel(BaseVoteFieldModel):
+    """
+    Модель вопрос -> голосование
+    """
     vote_form = models.ForeignKey(VotingFormModel,on_delete=models.PROTECT)
 
 class SurveyFieldModel(BaseVoteFieldModel):
+    """
+    Модель вопрос -> опрос
+    """
     vote_form = models.ForeignKey(SurveyFormModel,on_delete=models.PROTECT)
+
+class SurveyUserRelationship(models.Model):
+    """
+    Модель юзер -> опрос
+    """
+    survey = models.ForeignKey(SurveyFormModel,on_delete=models.CASCADE)
+    user = models.ForeignKey(UserModel,on_delete=models.CASCADE)
+
+class VoteUserRelationship(models.Model):
+    """
+    Модель юзер -> голосование
+    """
+    vote = models.ForeignKey(VotingFormModel,on_delete=models.CASCADE)
+    user = models.ForeignKey(UserModel,on_delete=models.CASCADE)
